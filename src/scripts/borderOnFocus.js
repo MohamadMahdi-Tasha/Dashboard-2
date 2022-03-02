22dabbc6da98ca57a34710c6f39fc1a813eb0cfc
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2022-03-01 19:53:02 -0800

    added border on focus and blur of main sections search input

---
+++ src/scripts/borderOnFocus.js
@@ -0,0 +1,9 @@
+// Codes By Mahdi Tasha
+// Variables
+const search_input = document.getElementById('search-input');
+const search_input_holder = document.getElementById('search-input-holder');
+
+// Adding Event Listener To Search Input Of Top Side Of Middle Section For
+// Focus And Blur And It Borders Changes
+search_input.addEventListener('focus', () => {search_input_holder.style.border = '1px solid #5f84f6';})
+search_input.addEventListener('blur', () => {search_input_holder.style.border = '1px solid #f5f6fb';})
